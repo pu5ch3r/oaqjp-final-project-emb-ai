e1785821d5a8293be7c77fd6ef8ec275dec36849
Christian Pesch <pu5ch3r@gmail.com>
2026-04-01 17:20:20 -0400

    task 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,6 +9,10 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(EMOTION_URL, json=input, headers=HEADERS)
 
+    if response.status_code == 400:
+        dict_emotions = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+        return dict_emotions
+    
     json_response = json.loads(response.text)
     json_emotions = dict(json_response["emotionPredictions"][0]["emotion"])
 
@@ -17,9 +21,7 @@ def emotion_detector(text_to_analyze):
     fear = json_emotions["fear"]
     joy = json_emotions["joy"]
     sadness = json_emotions["sadness"]
-
     dict_emotions = {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness}
-    dominant_emotion = max(dict_emotions, key=dict_emotions.get)
+    dominant_emotion = max(dict_emotions, key=dict_emotions.get)    
     dict_emotions["dominant_emotion"] = dominant_emotion
-
     return dict_emotions
--- server.py
+++ server.py
@@ -1,14 +1,27 @@
+'''
+   detecting emotions for user provided strings
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def detect_emotions():
+    '''
+       handling user specific input and calling the emotion detector
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
-    emotions = emotion_detector(text_to_analyze)
-    return emotions
+    if text_to_analyze != "":
+        emotions = emotion_detector(text_to_analyze)
+
+        if emotions["dominant_emotion"] is not None:
+            return emotions
+    return {"message": "Invalid text! Please try again!"}
 
 @app.route("/")
 def home():
+    '''
+        returning the static home page
+    '''
     return render_template("index.html")
